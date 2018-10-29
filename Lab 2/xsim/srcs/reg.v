cdde391b4066e0a7fed1b175c65145a07cb7c7c1
George Suarez <006098556@csusb.edu>
2018-10-29 12:37:25 -0700

    Added timing diagram for reg.v

--- Lab 2/xsim/srcs/reg.v
+++ Lab 2/xsim/srcs/reg.v
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ps
 
 module register ( 
-    input [4:0] rs,
+    input [4:0] rs,         
     input [4:0] rt,
     input [4:0] rd,
     input [31:0] write_data,
     input reg_write,
-    output reg [31:0] A,
-    output reg [31:0] B
+    output reg [31:0] A,    // rs output
+    output reg [31:0] B     // rd output
   );
   
   // Register declaration
@@ -23,14 +23,14 @@ module register (
     for (i = 0; i < 32; i = i + 1)
         REG[i] <= 0;
         
-    // Display contents of the first 9 registers
-    $display("From Register Memory:");
+        // Display contents of the first 9 registers
+        $display("From Register Memory:");
         for (i = 0; i < 9; i = i + 1)
             $display("\tREG[%0d] = %0d", i, REG[i]);
             
-    // Display last register
-    $display("\t...");
-    $display("\tREG[%0d] = %0d", 31, REG[31]);
+            // Display last register
+            $display("\t...");
+            $display("\tREG[%0d] = %0d", 31, REG[31]);
   end
   
   always @ * begin
@@ -39,9 +39,7 @@ module register (
     
     // Write data using index rd
     if (rd != 0 && reg_write)
-    begin
         REG[rd] <= write_data;
     end
-  end
   
-endmodule
+endmodule // register
--- Lab 2/xsim/srcs/register_tb.v
+++ Lab 2/xsim/srcs/register_tb.v
@@ -1,39 +1,50 @@
-module test();
+`timescale 1ns / 1ps
+
+module test;
     
-    // Port Wires
-    wire [31:0] rdata1, rdata2, wdata;
-    wire [4:0] wreg;
+    // Outputs
     wire [31:0] A, B;
-    wire reg_write;
-    wire [31:0] rreg1, rreg2;
     
-    // Register Declarations
-    reg [31:0] register[0:31];
-       
-    initial begin
-        $display("Time\tRead data 1\tRead Data 2\n");
-        $monitor("%0d\t%h\t%h", $time, rdata1, rdata2);
-        #20; $finish;
+    // Inputs
+    reg [4:0] rs, rt, rd;
+    reg [31:0] write_data;
+    reg reg_write;
+    
+    register uut( .rs( rs ),
+                  .rt( rt ),
+                  .rd( rd ),
+                  .write_data( write_data ),
+                  .reg_write( reg_write ),
+                  .A( A ),
+                  .B( B ) );
+    
+    initial
+    begin
+        rs = 0;
+        rt = 1;
+        reg_write = 0;
+        
+        #1 
+        rs = 2;
+        rt = 3;
+        rd = 3;
+        write_data = 100;
+        
+        #1 
+        rs = 4;
+        rt = 5;
+        reg_write = 1;
+        
+        #1 
+        reg_write = 0;
+        rt = 3;
+        
+        #1 
+        rs = 6;
+        reg_write = 1;
+        rd = 6;
+        write_data = 100;
+        
+        #1 $finish;
     end
-    
-    always @ ( reg_write ) begin
-        #1 register[0] <= 'h002300AA;
-        #1 register[1] <= 'h10654321;
-        #1 register[2] <= 'h00100022;
-        #1 register[3] <= 'h8C123456;
-        #1 register[4] <= 'h8F123456;
-        #1 register[5] <= 'hAD654321;
-        #1 register[6] <= 'h13012345;
-        #1 register[7] <= 'hAC654321;
-        #1 register[8] <= 'h12012345;
-    end
-    
-    register reg1( .rs( rreg1[25:21] ),
-                   .rt( rreg2[20:16] ),
-                   .rd( wreg ),
-                   .write_data( wdata ),
-                   .reg_write( reg_write ),
-                   .A( rdata1 ),
-                   .B( rdata2 ) );
-    
 endmodule
